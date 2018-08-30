e4d63ca0dcfa3b70ccfd21a81098b296f4cf7eb4
Politias Castle <Politias Castle@DESKTOP-A2GKNCL>
2018-08-30 12:35:11 +0200

    First test uppload with empty project.

---
+++ Lab/src/Lab.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Lab.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	return EXIT_SUCCESS;
+}
